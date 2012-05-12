5690bbcd4db84cf5c32c3a4c7e8c9f7a90427da5
Andreas Würl <andreas@wuerl.net>
2012-05-12 08:51:01 +0200

    updated colors fixed german translation

--- src/org/blitzortung/android/map/overlay/color/StrokeColorHandler.java
+++ src/org/blitzortung/android/map/overlay/color/StrokeColorHandler.java
@@ -4,9 +4,9 @@ import android.content.SharedPreferences;
 
 public class StrokeColorHandler extends ColorHandler {
 	
-	private int[] colorsOnSatelliteView = { 0xffff0000, 0xffff9900, 0xffffff00, 0xff99ff22, 0xff00ffff, 0xff99bbff };
+	private int[] colorsOnSatelliteView = { 0xffff0000, 0xffff9900, 0xffffff00, 0xff99ff22, 0xff00ffff, 0xff6699ff };
 	
-	private int[] mapColors = { 0xff880000, 0xff884400, 0xff888800, 0xff448811, 0xff008888, 0xff222288 };
+	private int[] mapColors = { 0xffcc2200, 0xffaa5500, 0xffaaaa00, 0xff44aa11, 0xff00aaaa, 0xff2222cc };
 
 	public StrokeColorHandler(SharedPreferences preferences) {
 		super(preferences);
