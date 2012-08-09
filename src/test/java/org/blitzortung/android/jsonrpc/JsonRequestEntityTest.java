f89df0a4c66ddb5ef9586a3c66222e156d0a6ff5
Andreas Wuerl <blitzortung@tryb.de>
2012-08-09 21:54:45 +0200

    fixed jsonrequestentity test

---
+++ src/test/java/org/blitzortung/android/jsonrpc/JsonRequestEntityTest.java
@@ -0,0 +1,48 @@
+package org.blitzortung.android.jsonrpc;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(RobolectricTestRunner.class)
+public class JsonRequestEntityTest {
+
+    private JsonRequestEntity jsonRequestEntity;
+
+    @Mock
+    private JSONObject jsonObject;
+
+    @Before
+    public void setUp() throws UnsupportedEncodingException, JSONException {
+        MockitoAnnotations.initMocks(this);
+
+        when(jsonObject.toString()).thenReturn("{}");
+        //jsonObject = new JSONObject("{}");
+
+        jsonRequestEntity = new JsonRequestEntity(jsonObject);
+    }
+
+    @Test
+    public void testGetContentType()
+    {
+        Header header = jsonRequestEntity.getContentType();
+
+        assertThat(header.getName(), is("Content-Type"));
+        assertThat(header.getValue(), is("text/json"));
+    }
+
+
+}
