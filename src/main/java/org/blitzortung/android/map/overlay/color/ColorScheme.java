a4ac469ef75d6f17e4e2de18b69e9755e4fbb02d
wuan <andreas@wuerl.net>
2012-11-20 15:13:16 +0100

    switched to original blitzortung.org color scheme

--- src/main/java/org/blitzortung/android/map/overlay/color/ColorScheme.java
+++ src/main/java/org/blitzortung/android/map/overlay/color/ColorScheme.java
@@ -1,7 +1,7 @@
 package org.blitzortung.android.map.overlay.color;
 
 public enum ColorScheme {
-    BLITZORTUNG(new int[]{ 0xffffffff, 0xffffff00, 0xffffaa00, 0xffff6600, 0xffbb4400, 0xff882200 }),
+    BLITZORTUNG(new int[]{ 0xffe4f9f9, 0xffd8f360, 0xffdfbc51, 0xffe48044, 0xffe73c3b, 0xffb82e2d }),
     RAINBOW(new int[]{ 0xffff0000, 0xffff9900, 0xffffff00, 0xff99ff22, 0xff00ffff, 0xff6699ff });
 
     private final int[] colors;
