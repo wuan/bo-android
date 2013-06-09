9977ce2d038e300ff955df9bd6d851392f58bcac
Andreas Würl <andi@tryb.de>
2013-06-09 22:19:33 +0200

    fix mapview

--- src/main/java/org/blitzortung/android/map/OwnMapActivity.java
+++ src/main/java/org/blitzortung/android/map/OwnMapActivity.java
@@ -1,24 +1,27 @@
 package org.blitzortung.android.map;
 
+import android.util.Log;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.Overlay;
+import org.blitzortung.android.app.Main;
 import org.blitzortung.android.map.overlay.LayerOverlay;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class OwnMapActivity extends MapActivity {
 
-	private OwnMapView mapView;
-    
+    private OwnMapView mapView;
+
     List<Overlay> overlays = new ArrayList<Overlay>();
-    
+
     public void setMapView(OwnMapView mapView) {
-    	this.mapView = mapView;
+        this.mapView = mapView;
     }
-    
+
     public OwnMapView getMapView() {
-    	return mapView;
+        return mapView;
     }
 
     public void addOverlays(Overlay... addedOverlays) {
@@ -27,25 +30,45 @@ public abstract class OwnMapActivity extends MapActivity {
         }
         updateOverlays();
     }
-    
-    public void updateOverlays()
-    {
+
+    public void updateOverlays() {
         List<Overlay> mapOverlays = mapView.getOverlays();
-        
+
         mapOverlays.clear();
-        
+
         for (Overlay overlay : overlays) {
             boolean enabled = true;
-            
+
             if (overlay instanceof LayerOverlay) {
-                enabled = ((LayerOverlay)overlay).isEnabled();
+                enabled = ((LayerOverlay) overlay).isEnabled();
             }
-            
+
             if (enabled) {
                 mapOverlays.add(overlay);
             }
-        } 
-        
+        }
+
         mapView.invalidate();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        try {
+            Field mConfigField = MapActivity.class.getDeclaredField("mConfig");
+            mConfigField.setAccessible(true);
+
+            Object mConfig = mConfigField.get(this);
+            if (null != mConfig) {
+                Field mConfigContextField = mConfig.getClass().getDeclaredField("context");
+                mConfigContextField.setAccessible(true);
+                mConfigContextField.set(mConfig, null);
+                mConfigField.set(this, null);
+            }
+        } catch (Exception e) {
+            Log.w(Main.LOG_TAG, "OwnMapActivity.onDestroy() failed");
+        }
+
     }
 }
