d088949d84b118948f7c8683208a5debd52888f7
Andreas Würl <andi@tryb.de>
2015-10-25 02:24:38 +0100

    more lombok

--- app/src/main/java/org/blitzortung/android/location/LocationEvent.java
+++ app/src/main/java/org/blitzortung/android/location/LocationEvent.java
@@ -3,15 +3,10 @@ package org.blitzortung.android.location;
 import android.location.Location;
 import org.blitzortung.android.protocol.Event;
 
+import lombok.Value;
+
+@Value
 public class LocationEvent implements Event {
 
     private final Location location;
-
-    public LocationEvent(final Location location) {
-        this.location = location;
-    }
-
-    public Location getLocation() {
-        return location;
-    }
 }
