1cc6c3ae39e0824b23434bf908cf32f3d01735ed
Andreas Sinz <andreas.sinz@aon.at>
2016-04-05 13:08:24 +0200

    Make use of some kotlin features to allow retrieving data only when credentials are set and setUp/shutdown is called

--- app/src/main/java/org/blitzortung/android/data/provider/standard/JsonRpcDataProvider.kt
+++ app/src/main/java/org/blitzortung/android/data/provider/standard/JsonRpcDataProvider.kt
@@ -39,7 +39,6 @@ class JsonRpcDataProvider(serviceUrl : String? = null) : DataProvider() {
 
     private val serviceUrl: String
     private val dataBuilder: DataBuilder
-    private var client: JsonRpcClient? = null
     private var nextId = 0
 
     init {
@@ -50,86 +49,18 @@ class JsonRpcDataProvider(serviceUrl : String? = null) : DataProvider() {
         Log.v(Main.LOG_TAG, "JsonRpcDataProvider(${this.serviceUrl})")
     }
 
-    override fun getStrikes(parameters: Parameters, result: ResultEvent): ResultEvent {
-        var result = result
-        val intervalDuration = parameters.intervalDuration
-        val intervalOffset = parameters.intervalOffset
-        if (intervalOffset < 0) {
-            nextId = 0
-        }
-        result = result.copy(incrementalData = (nextId != 0))
-
-        try {
-            val response = client!!.call("get_strikes", intervalDuration, if (intervalOffset < 0) intervalOffset else nextId)
-
-            result = addStrikes(response, result)
-            result = addStrikesHistogram(response, result)
-        } catch (e: Exception) {
-            throw RuntimeException(e)
-        }
-
-        Log.v(Main.LOG_TAG,
-                "JsonRpcDataProvider: read %d bytes (%d new strikes)".format(client!!.lastNumberOfTransferredBytes, result.strikes?.size))
-        return result
-    }
-
-    override fun getStrikesGrid(parameters: Parameters, resultParam: ResultEvent): ResultEvent {
-        var result = resultParam
-
-        nextId = 0
-
-        val intervalDuration = parameters.intervalDuration
-        val intervalOffset = parameters.intervalOffset
-        val rasterBaselength = parameters.rasterBaselength
-        val countThreshold = parameters.countThreshold
-        val region = parameters.region
-
-        try {
-            val response = client!!.call("get_strikes_grid", intervalDuration, rasterBaselength, intervalOffset, region, countThreshold)
-
-            val info = "%.0f km".format(rasterBaselength / 1000f)
-            result = addRasterData(response, result, info)
-            result = addStrikesHistogram(response, result)
-        } catch (e: Exception) {
-            throw RuntimeException(e)
-        }
-
-        Log.v(Main.LOG_TAG,
-                "JsonRpcDataProvider: read %d bytes (%d raster positions, region %d)".format(client!!.lastNumberOfTransferredBytes, result.strikes?.size, region))
-        return result
-    }
-
-    override fun getStations(region: Int): List<Station> {
-        val stations = ArrayList<Station>()
-
-        try {
-            val response = client!!.call("get_stations")
-            val stations_array = response.get("stations") as JSONArray
-
-            for (i in 0..stations_array.length() - 1) {
-                stations.add(dataBuilder.createStation(stations_array.getJSONArray(i)))
-            }
-        } catch (e: Exception) {
-            throw RuntimeException(e)
-        }
-
-        return stations
-    }
-
     override val type: DataProviderType
         get() = DataProviderType.RPC
 
-    override fun setUp() {
+    private fun setUpClient(): JsonRpcClient {
         val pInfo = pInfo
         val agentSuffix = if (pInfo != null) "-" + Integer.toString(pInfo.versionCode) else ""
-        client = JsonRpcClient(serviceUrl, agentSuffix)
-        client!!.connectionTimeout = 40000
-        client!!.socketTimeout = 40000
-    }
+        val client = JsonRpcClient(serviceUrl, agentSuffix)
 
-    override fun shutDown() {
-        client!!.shutdown()
-        client = null
+        return client.apply {
+            connectionTimeout = 40000
+            socketTimeout = 40000
+        }
     }
 
     override fun reset() {
@@ -188,6 +119,87 @@ class JsonRpcDataProvider(serviceUrl : String? = null) : DataProvider() {
 
         return result
     }
+
+    override fun <T> retrieveData(username: String?, password: String?, retrieve: DataRetriever.() -> T): T {
+        val client = setUpClient()
+
+        val t = Retriever(client).retrieve()
+
+        client.shutdown()
+
+        return t
+    }
+
+
+    private inner class Retriever(val client: JsonRpcClient): DataRetriever {
+        override fun getStations(region: Int): List<Station> {
+            val stations = ArrayList<Station>()
+
+            try {
+                val response = client.call("get_stations")
+                val stations_array = response.get("stations") as JSONArray
+
+                for (i in 0..stations_array.length() - 1) {
+                    stations.add(dataBuilder.createStation(stations_array.getJSONArray(i)))
+                }
+            } catch (e: Exception) {
+                throw RuntimeException(e)
+            }
+
+            return stations
+        }
+
+        override fun getStrikesGrid(parameters: Parameters, result: ResultEvent): ResultEvent {
+            var result = result
+
+            nextId = 0
+
+            val intervalDuration = parameters.intervalDuration
+            val intervalOffset = parameters.intervalOffset
+            val rasterBaselength = parameters.rasterBaselength
+            val countThreshold = parameters.countThreshold
+            val region = parameters.region
+
+            try {
+                val response = client.call("get_strikes_grid", intervalDuration, rasterBaselength, intervalOffset, region, countThreshold)
+
+                val info = "%.0f km".format(rasterBaselength / 1000f)
+                result = addRasterData(response, result, info)
+                result = addStrikesHistogram(response, result)
+            } catch (e: Exception) {
+                throw RuntimeException(e)
+            }
+
+            Log.v(Main.LOG_TAG,
+                    "JsonRpcDataProvider: read %d bytes (%d raster positions, region %d)".format(client.lastNumberOfTransferredBytes, result.strikes?.size, region))
+            return result
+        }
+
+        override fun getStrikes(parameters: Parameters, result: ResultEvent): ResultEvent {
+            var result = result
+            val intervalDuration = parameters.intervalDuration
+            val intervalOffset = parameters.intervalOffset
+            if (intervalOffset < 0) {
+                nextId = 0
+            }
+            result = result.copy(incrementalData = (nextId != 0))
+
+            try {
+                val response = client.call("get_strikes", intervalDuration, if (intervalOffset < 0) intervalOffset else nextId)
+
+                result = addStrikes(response, result)
+                result = addStrikesHistogram(response, result)
+            } catch (e: Exception) {
+                throw RuntimeException(e)
+            }
+
+            Log.v(Main.LOG_TAG,
+                    "JsonRpcDataProvider: read %d bytes (%d new strikes)".format(client.lastNumberOfTransferredBytes, result.strikes?.size))
+            return result
+        }
+
+    }
+
 
     companion object {
         private val DATE_TIME_FORMATTER = SimpleDateFormat("yyyyMMdd'T'HH:mm:ss")
