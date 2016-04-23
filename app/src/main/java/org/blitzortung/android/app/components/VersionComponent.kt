e5241c2f9ff938c4663d55256c0e862640c2db65
Andreas Würl <andi@tryb.de>
2016-04-23 02:20:29 +0200

    refactored and tested VersionComponent

--- app/src/main/java/org/blitzortung/android/app/components/VersionComponent.kt
+++ app/src/main/java/org/blitzortung/android/app/components/VersionComponent.kt
@@ -20,6 +20,7 @@ package org.blitzortung.android.app.components
 
 import android.content.Context
 import android.util.Log
+import org.blitzortung.android.app.BuildConfig
 import org.blitzortung.android.app.Main
 
 class VersionComponent(context: Context) {
@@ -27,21 +28,14 @@ class VersionComponent(context: Context) {
     var state: State? = null
         private set
 
-    var versionName: String? = null
-        private set
-
     var configuredVersionCode: Int = 0
         private set
 
-    var versionCode: Int = 0
-        private set
+    val versionCode = BuildConfig.VERSION_CODE
+
+    var versionName = BuildConfig.VERSION_NAME
 
     init {
-        updatePackageInfo(context)
-        updateVersionStatus(context)
-    }
-
-    private fun updateVersionStatus(context: Context) {
         val packageName = context.packageName
         val preferences = context.getSharedPreferences(packageName, Context.MODE_PRIVATE)
         configuredVersionCode = preferences.getInt(CONFIGURED_VERSION_CODE, -1)
@@ -57,18 +51,11 @@ class VersionComponent(context: Context) {
         Log.d(Main.LOG_TAG, "updateVersionStatus() name=$packageName, state=$state, configuredVersion=$configuredVersionCode, currentVersion=$versionCode")
     }
 
-    private fun updatePackageInfo(context: Context) {
-        val pInfo = context.packageManager.getPackageInfo(context.packageName, 0)
-
-        versionCode = pInfo.versionCode
-        versionName = pInfo.versionName
-    }
-
     enum class State {
         FIRST_RUN, FIRST_RUN_AFTER_UPDATE, NO_UPDATE
     }
 
     companion object {
-        private val CONFIGURED_VERSION_CODE = "configured_version_code"
+        internal val CONFIGURED_VERSION_CODE = "configured_version_code"
     }
 }
---
+++ app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
@@ -0,0 +1,65 @@
+package org.blitzortung.android.app.components
+
+import android.content.Context
+import org.assertj.core.api.KotlinAssertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest=Config.NONE)
+class VersionComponentTest {
+
+    lateinit private var versionComponent: VersionComponent
+
+    @Before
+    fun setUp() {
+        versionComponent = VersionComponent(RuntimeEnvironment.application)
+    }
+
+    @Test
+    fun configuredVersionCodeShouldBeMinusOneWhenUndefied() {
+        assertThat(versionComponent.configuredVersionCode).isEqualTo(-1)
+    }
+
+    @Test
+    fun stateShouldBeFirstRunWhenConfiguredVersionIsUndefined() {
+       assertThat(versionComponent.state).isEqualTo(VersionComponent.State.FIRST_RUN)
+    }
+
+    @Test
+    fun shouldReturnVersionCode() {
+        assertThat(versionComponent.versionCode).isEqualTo(CURRENT_VERSION_CODE)
+    }
+
+    @Test
+    fun shouldReturnVersionName() {
+        assertThat(versionComponent.versionName).isEqualTo(CURRENT_VERSION_NAME)
+    }
+
+    @Test
+    fun shouldReturnNoUpdateStateWhenCalledNextTime() {
+        versionComponent = VersionComponent(RuntimeEnvironment.application)
+        assertThat(versionComponent.configuredVersionCode).isEqualTo(CURRENT_VERSION_CODE)
+        assertThat(versionComponent.state).isEqualTo(VersionComponent.State.NO_UPDATE)
+    }
+
+    @Test
+    fun shouldReturnUpdatedStateWhenCalledFirstTimeAfterVersionChange() {
+        val preferences = RuntimeEnvironment.application.getSharedPreferences(RuntimeEnvironment.application.packageName, Context.MODE_PRIVATE)
+        preferences
+                .edit().putInt(VersionComponent.CONFIGURED_VERSION_CODE, 1).apply()
+
+        versionComponent = VersionComponent(RuntimeEnvironment.application)
+        assertThat(versionComponent.configuredVersionCode).isEqualTo(1)
+        assertThat(versionComponent.state).isEqualTo(VersionComponent.State.FIRST_RUN_AFTER_UPDATE)
+    }
+
+    companion object {
+        val CURRENT_VERSION_CODE = 169
+        val CURRENT_VERSION_NAME = "1.5.0"
+    }
+}
