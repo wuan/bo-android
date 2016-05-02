64c4ea3bc8da3729179b18d1eff867222961055e
Andreas Würl <andi@tryb.de>
2016-05-02 21:29:53 +0200

    prepare for new version

--- app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
+++ app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
@@ -61,7 +61,7 @@ class VersionComponentTest {
     }
 
     companion object {
-        val CURRENT_VERSION_CODE = 169
-        val CURRENT_VERSION_NAME = "1.5.0"
+        val CURRENT_VERSION_CODE = 170
+        val CURRENT_VERSION_NAME = "1.5.1"
     }
 }
