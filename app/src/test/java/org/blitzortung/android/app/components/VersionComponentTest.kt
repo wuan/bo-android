addbb2cffa41fbb82cf71527c34ffbd01848f18c
Andreas Würl <andi@tryb.de>
2017-07-17 00:05:49 +0200

    fixed broken tests

--- app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
+++ app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
@@ -61,7 +61,7 @@ class VersionComponentTest {
     }
 
     companion object {
-        val CURRENT_VERSION_CODE = 191
-        val CURRENT_VERSION_NAME = "1.5.19"
+        val CURRENT_VERSION_CODE = 192
+        val CURRENT_VERSION_NAME = "1.6-beta1"
     }
 }
