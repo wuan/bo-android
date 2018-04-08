e713359a8da345e57f6e4e37f8dd1401f92c3e34
Andreas Würl <andi@tryb.de>
2018-04-08 23:47:53 +0200

    fix version

--- app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
+++ app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
@@ -61,7 +61,7 @@ class VersionComponentTest {
     }
 
     companion object {
-        val CURRENT_VERSION_CODE = 197
+        val CURRENT_VERSION_CODE = 196
         val CURRENT_VERSION_NAME = "1.6-beta4"
     }
 }
