2964123d130085df3459b96f69c6391f145532da
Andreas Würl <andi@tryb.de>
2018-05-02 23:26:04 +0200

    prepare release and update dependency versions

--- app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
+++ app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
@@ -61,7 +61,7 @@ class VersionComponentTest {
     }
 
     companion object {
-        val CURRENT_VERSION_CODE = 199
-        val CURRENT_VERSION_NAME = "1.6.0"
+        val CURRENT_VERSION_CODE = 200
+        val CURRENT_VERSION_NAME = "1.6.1"
     }
 }
