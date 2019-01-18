9a88744241ee3ec0014083bb13d43e762caf20b2
Andreas Wuerl <andi@tryb.de>
2019-01-18 23:03:03 +0100

    introduce separate string for app log email subject

--- app/src/main/java/org/blitzortung/android/dialogs/LogDialog.kt
+++ app/src/main/java/org/blitzortung/android/dialogs/LogDialog.kt
@@ -82,7 +82,7 @@ class LogDialog(
         val intent = Intent(Intent.ACTION_SENDTO)
         intent.data = Uri.parse("mailto:")
         intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(context.resources.getString(R.string.project_email)))
-        intent.putExtra(Intent.EXTRA_SUBJECT, context.resources.getString(R.string.app_log))
+        intent.putExtra(Intent.EXTRA_SUBJECT, context.resources.getString(R.string.app_log_subject))
         intent.putExtra(Intent.EXTRA_TEXT, logText)
         if (intent.resolveActivity(context.packageManager) != null) {
             context.startActivity(intent)
