d366b4ff02c2ffd1bd809631b829039ba72d61bf
Andreas Wuerl <andi@tryb.de>
2019-01-19 01:08:30 +0100

    updates

--- app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
+++ app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
@@ -61,7 +61,7 @@ class VersionComponentTest {
     }
 
     companion object {
-        val CURRENT_VERSION_CODE = 206
-        val CURRENT_VERSION_NAME = "1.6.3"
+        val CURRENT_VERSION_CODE = 207
+        val CURRENT_VERSION_NAME = "1.6.4"
     }
 }
