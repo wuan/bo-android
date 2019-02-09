1062da770801c12a6fd473700d67340ce8c5ce1f
Andreas Wuerl <andi@tryb.de>
2019-02-09 17:23:13 +0100

    prepare release

--- app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
+++ app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
@@ -61,7 +61,7 @@ class VersionComponentTest {
     }
 
     companion object {
-        val CURRENT_VERSION_CODE = 220
+        val CURRENT_VERSION_CODE = 223
         val CURRENT_VERSION_NAME = "2.0.2"
     }
 }
