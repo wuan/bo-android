9d4f2b0b61277247f9e5a45a75f3ea38a251904a
Andreas Wuerl <andi@tryb.de>
2019-05-26 20:44:22 +0200

    update data service target and prepare release

--- app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
+++ app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
@@ -61,7 +61,7 @@ class VersionComponentTest {
     }
 
     companion object {
-        val CURRENT_VERSION_CODE = 234
-        val CURRENT_VERSION_NAME = "2.0.8"
+        val CURRENT_VERSION_CODE = 235
+        val CURRENT_VERSION_NAME = "2.0.9"
     }
 }
