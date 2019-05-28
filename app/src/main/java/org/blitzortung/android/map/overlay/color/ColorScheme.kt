dccfba33bf16f90fb65ca3c7d60ce0a7e1fe4f55
Andreas Wuerl <andi@tryb.de>
2019-05-28 20:16:48 +0200

    brighter blitzortung color palette

--- app/src/main/java/org/blitzortung/android/map/overlay/color/ColorScheme.kt
+++ app/src/main/java/org/blitzortung/android/map/overlay/color/ColorScheme.kt
@@ -19,7 +19,7 @@
 package org.blitzortung.android.map.overlay.color
 
 enum class ColorScheme(val strikeColors: IntArray) {
-    BLITZORTUNG(intArrayOf(0xffe4f9f9.toInt(), 0xffd8f360.toInt(), 0xffdfbc51.toInt(), 0xffe48044.toInt(), 0xffe73c3b.toInt(), 0xffb82e2d.toInt())),
+    BLITZORTUNG(intArrayOf(0xffffffff.toInt(), 0xffffffb0.toInt(), 0xffffea80.toInt(), 0xfff0b060.toInt(), 0xffd06040.toInt(), 0xffc03030.toInt())),
     RAINBOW(intArrayOf(0xffff0000.toInt(), 0xffff9900.toInt(), 0xffffff00.toInt(), 0xff99ff22.toInt(), 0xff00ffff.toInt(), 0xff6699ff.toInt())),
     TEMPERATURE(intArrayOf(0xffff5030.toInt(), 0xffe78060.toInt(), 0xffe0b0c0.toInt(), 0xffc0c0f0.toInt(), 0xff5080f0.toInt(), 0xff4060ff.toInt()));
 }
