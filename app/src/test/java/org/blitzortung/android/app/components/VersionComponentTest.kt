ff9c58074940ff1c596f420ecd75381f96272137
Andreas Wuerl <andi@tryb.de>
2019-09-14 09:52:43 +0200

    update version

--- app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
+++ app/src/test/java/org/blitzortung/android/app/components/VersionComponentTest.kt
@@ -61,7 +61,7 @@ class VersionComponentTest {
     }
 
     companion object {
-        val CURRENT_VERSION_CODE = 245
+        val CURRENT_VERSION_CODE = 246
         val CURRENT_VERSION_NAME = "2.0.15"
     }
 }
