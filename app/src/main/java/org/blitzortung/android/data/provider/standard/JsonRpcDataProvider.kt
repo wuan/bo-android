faead9abaf001b29ea8ea67f0fded8ce57c49a55
Andreas Würl <andi@tryb.de>
2022-08-06 15:33:25 +0200

    remove network error toasts (fixes #216)

--- app/src/main/java/org/blitzortung/android/data/provider/standard/JsonRpcDataProvider.kt
+++ app/src/main/java/org/blitzortung/android/data/provider/standard/JsonRpcDataProvider.kt
@@ -1,6 +1,6 @@
 /*
 
-   Copyright 2015 Andreas Würl
+   Copyright 2015-2022 Andreas Würl
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
