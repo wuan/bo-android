716fc65cafb777019408f7c79de14fdf894a8a07
Ivan <ivan@lepton.ru>
2023-10-23 13:55:56 +0300

    Add translation for "ms" strings.

--- app/src/main/java/org/blitzortung/android/app/view/HistogramView.kt
+++ app/src/main/java/org/blitzortung/android/app/view/HistogramView.kt
@@ -97,7 +97,7 @@ class HistogramView @JvmOverloads constructor(
             val maximumCount = histogram.maxOrNull() ?: 0
 
             canvas.drawText(
-                "%.1f/%s".format(
+                "%.1f/%s _".format(
                     maximumCount.toFloat() / minutesPerBin, resources.getString(R.string.unit_minute)
                 ), width - 2 * padding, padding + textSize / 1.2f, textPaint
             )
