b7fa8904fb0e4a26be25dc07bb2e2af110e34781
Andreas Würl <andi@tryb.de>
2025-07-31 23:52:18 +0200

    fixing preferences layout

--- app/src/main/java/org/blitzortung/android/settings/SettingsFragment.kt
+++ app/src/main/java/org/blitzortung/android/settings/SettingsFragment.kt
@@ -2,17 +2,26 @@ package org.blitzortung.android.settings
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.WindowCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.preference.PreferenceFragmentCompat
 import org.blitzortung.android.app.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
-    public override fun onCreatePreferences(bundle: Bundle?, s: String?) {
+    override fun onCreatePreferences(bundle: Bundle?, s: String?) {
         addPreferencesFromResource(R.xml.preferences)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(requireActivity().window, true)
+        // Remove: WindowCompat.setDecorFitsSystemWindows(requireActivity().window, true)
+
+        // Add padding to the RecyclerView to account for system bars
+        val recyclerView = listView
+        ViewCompat.setOnApplyWindowInsetsListener(recyclerView) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
