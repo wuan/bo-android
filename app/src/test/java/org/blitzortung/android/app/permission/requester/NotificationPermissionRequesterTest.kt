dae915bf10298b6d0066517549173baf3e468ea9
Andreas Würl <andi@tryb.de>
2025-09-07 18:32:19 +0200

    add tests for Notification permission callback

--- app/src/test/java/org/blitzortung/android/app/permission/requester/NotificationPermissionRequesterTest.kt
+++ app/src/test/java/org/blitzortung/android/app/permission/requester/NotificationPermissionRequesterTest.kt
@@ -5,6 +5,7 @@ import android.Manifest.permission.POST_NOTIFICATIONS
 import android.app.Activity
 import android.content.Context
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.core.content.edit
 import io.mockk.MockKAnnotations
@@ -16,6 +17,7 @@ import org.blitzortung.android.app.Main
 import org.blitzortung.android.app.R
 import org.blitzortung.android.app.permission.PermissionsSupport
 import org.blitzortung.android.app.view.PreferenceKey
+import org.blitzortung.android.app.view.get
 import org.blitzortung.android.app.view.put
 import org.junit.Before
 import org.junit.Test
@@ -66,8 +68,8 @@ class NotificationPermissionRequesterTest {
     }
 
     @Test
-    @Config(sdk = [Build.VERSION_CODES.TIRAMISU]) // Test on SDK < TIRAMISU (33)
-    fun `request should return false if SDK is above TIRAMISU and alert is not enabled`() {
+    @Config(sdk = [Build.VERSION_CODES.TIRAMISU])
+    fun `request should return false if SDK is TIRAMISU or above and alert is not enabled`() {
         every { permissionsSupport.request(any(), any(), any()) } answers { true }
 
         val result = notificationPermissionRequester.request(permissionsSupport)
@@ -77,8 +79,8 @@ class NotificationPermissionRequesterTest {
     }
 
     @Test
-    @Config(sdk = [Build.VERSION_CODES.TIRAMISU]) // Test on SDK < TIRAMISU (33)
-    fun `request should return the true result of the requestPermission method if SDK is above TIRAMISU`() {
+    @Config(sdk = [Build.VERSION_CODES.TIRAMISU])
+    fun `request should return the true result of the requestPermission method if SDK is TIRAMISU or above`() {
         every { permissionsSupport.request(any(), any(), any()) } answers { true }
         preferences.edit {
             put(PreferenceKey.ALERT_ENABLED, true)
@@ -97,8 +99,8 @@ class NotificationPermissionRequesterTest {
     }
 
     @Test
-    @Config(sdk = [Build.VERSION_CODES.TIRAMISU]) // Test on SDK < TIRAMISU (33)
-    fun `request should return the false result of the requestPermission method if SDK is above TIRAMISU when Background queries are enabled`() {
+    @Config(sdk = [Build.VERSION_CODES.TIRAMISU])
+    fun `request should return true when background queries are enabled and SDK is TIRAMISU or above`() {
         every { permissionsSupport.request(any(), any(), any()) } answers { true }
         preferences.edit {
             put(PreferenceKey.BACKGROUND_QUERY_PERIOD, "300")
@@ -115,7 +117,77 @@ class NotificationPermissionRequesterTest {
             )
         }
     }
-    companion object {
-        const val REQUEST_CODE_POST_NOTIFICATIONS = 101
+
+    @Test
+    fun `onRequestPermissionsResult returns false if request code does not match`() {
+        val result = notificationPermissionRequester.onRequestPermissionsResult(
+            requestCode = 123,
+            permissions = arrayOf(POST_NOTIFICATIONS),
+            grantResults = intArrayOf(PackageManager.PERMISSION_GRANTED)
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `onRequestPermissionsResult returns false if grant results is empty`() {
+        val result = notificationPermissionRequester.onRequestPermissionsResult(
+            requestCode = 101,
+            permissions = arrayOf(POST_NOTIFICATIONS),
+            grantResults = intArrayOf()
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `onRequestPermissionsResult returns true if permission is granted`() {
+        preferences.edit {
+            put(PreferenceKey.ALERT_ENABLED, true)
+            put(PreferenceKey.BACKGROUND_QUERY_PERIOD, "300")
+        }
+
+        val result = notificationPermissionRequester.onRequestPermissionsResult(
+            requestCode = 101,
+            permissions = arrayOf(POST_NOTIFICATIONS),
+            grantResults = intArrayOf(PackageManager.PERMISSION_GRANTED)
+        )
+
+        assertThat(result).isTrue()
+
+        assertThat(preferences.get(PreferenceKey.ALERT_ENABLED, false)).isTrue()
+        assertThat(preferences.get(PreferenceKey.BACKGROUND_QUERY_PERIOD, "0")).isEqualTo("300")
+    }
+
+    @Test
+    fun `onRequestPermissionsResult returns true if permission is denied and alerts are enabled`() {
+        preferences.edit {
+            put(PreferenceKey.ALERT_ENABLED, true)
+        }
+
+        val result = notificationPermissionRequester.onRequestPermissionsResult(
+            requestCode = 101,
+            permissions = arrayOf(POST_NOTIFICATIONS),
+            grantResults = intArrayOf(PackageManager.PERMISSION_DENIED)
+        )
+
+        assertThat(result).isTrue()
+
+        assertThat(preferences.get(PreferenceKey.ALERT_ENABLED, true)).isFalse()
+    }
+
+    @Test
+    fun `onRequestPermissionsResult returns true if permission is denied and background queries are enabled`() {
+        preferences.edit {
+            put(PreferenceKey.BACKGROUND_QUERY_PERIOD, "300")
+        }
+
+        val result = notificationPermissionRequester.onRequestPermissionsResult(
+            requestCode = 101,
+            permissions = arrayOf(POST_NOTIFICATIONS),
+            grantResults = intArrayOf(PackageManager.PERMISSION_DENIED)
+        )
+
+        assertThat(result).isTrue()
+
+        assertThat(preferences.get(PreferenceKey.BACKGROUND_QUERY_PERIOD, "300")).isEqualTo("0")
     }
 }
