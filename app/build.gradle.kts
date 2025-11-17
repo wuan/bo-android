ccfaaca963f91c7754535f2312627b6b5fb17c8d
Andreas Würl <andi@tryb.de>
2025-11-17 20:36:53 +0100

    enable ProGuard/R8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
